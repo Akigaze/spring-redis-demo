96b37d613a971bc30a02084e86fdfceba76c3fb3
Akigaze <1175520196@qq.com>
2022-01-19 20:21:46 +0800

    add more redisTemplate operations

--- src/main/java/com/example/spring/redis/v1/ContainerController.java
+++ src/main/java/com/example/spring/redis/v1/ContainerController.java
@@ -10,12 +10,22 @@ public class ContainerController {
   private final ContainerService containerService;
 
   @PostMapping("")
-  public ContainerEntity create(@RequestBody ContainerEntity container){
-    return containerService.save(container);
+  public ContainerEntity create(@RequestBody ContainerEntity container, @RequestParam(defaultValue = "0") int secondsToLive) {
+    return containerService.save(container, secondsToLive);
   }
 
   @GetMapping("/{refNum}")
-  public ContainerEntity get(@PathVariable String refNum){
+  public ContainerEntity get(@PathVariable String refNum) {
     return containerService.get(refNum);
+  }
+
+  @DeleteMapping("/{refNum}")
+  public Boolean delete(@PathVariable String refNum) {
+    return containerService.delete(refNum);
+  }
+
+  @PutMapping("")
+  public ContainerEntity delete(@RequestBody ContainerEntity container) {
+    return containerService.update(container);
   }
 }
--- src/main/java/com/example/spring/redis/v1/ContainerService.java
+++ src/main/java/com/example/spring/redis/v1/ContainerService.java
@@ -5,21 +5,73 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @Service
 @RequiredArgsConstructor
 public class ContainerService {
   @Qualifier("containerRedisTemplate")
   private final RedisTemplate<String, ContainerEntity> redisTemplate;
 
-  //  @Cacheable(cacheNames = "container", key = "container.refNum")
   public ContainerEntity save(ContainerEntity container) {
+    if (containerExisted(container)) {
+      return null;
+    }
     redisTemplate.opsForValue().set(container.getRefNum(), container);
     return container;
   }
 
-  //  @Cacheable(cacheNames = "container", key = "container.refNum")
+  public ContainerEntity save(ContainerEntity container, int secondsToLive) {
+    if (secondsToLive > 0) {
+      if (containerExisted(container)) {
+        return null;
+      }
+      redisTemplate.opsForValue().set(container.getRefNum(), container, Duration.ofSeconds(secondsToLive));
+      return container;
+    }
+    return this.save(container);
+  }
+
+  private Boolean containerExisted(ContainerEntity container) {
+    return redisTemplate.hasKey(container.getRefNum());
+  }
+
   public ContainerEntity get(String refNum) {
     return redisTemplate.opsForValue().get(refNum);
 
   }
+
+  public Boolean delete(String refNum) {
+    return redisTemplate.delete(refNum);
+  }
+
+  public ContainerEntity update(ContainerEntity container) {
+    ContainerEntity existed = this.get(container.getRefNum());
+    if (existed == null) {
+      return null;
+    }
+    this.copyNonNullProperties(existed, container);
+    redisTemplate.opsForValue().set(existed.getRefNum(), existed);
+    return existed;
+  }
+
+  private void copyNonNullProperties(ContainerEntity existed, ContainerEntity updated) {
+    if (updated.getTenantId() != null) {
+      existed.setTenantId(updated.getTenantId());
+    }
+    if (updated.getScac() != null) {
+      existed.setScac(updated.getScac());
+    }
+    if (updated.getBookingNumber() != null) {
+      existed.setBookingNumber(updated.getBookingNumber());
+    }
+    if (updated.getContainerNumber() != null) {
+      existed.setContainerNumber(updated.getContainerNumber());
+    }
+    if (updated.getCreatedTime() != null) {
+      existed.setCreatedTime(updated.getCreatedTime());
+    }
+  }
+
+
 }
